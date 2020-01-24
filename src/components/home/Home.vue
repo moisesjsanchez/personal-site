d328ddfece0baba589461a30bf29a5849d915cd7
moisesjsanchez <moijsanchez@gmail.com>
2020-01-24 01:48:52 -0800

    initial commit

---
+++ src/components/home/Home.vue
@@ -0,0 +1,38 @@
+<template>
+
+<!-- Hero Image -->
+  <v-container>
+    <v-row>
+      <v-col class="text-center mt-5" cols="12">
+        <h1> MOISES SANCHEZ </h1>
+        <v-divider dark />
+        <h4> Welcome to My Webpage </h4>
+      </v-col>
+    </v-row>
+    <!-- Button Rows -->
+    <div class="text-center">
+      <v-col cols="12">
+        <v-btn class="ma-2" outlined color="black">
+          <v-icon small left class="fab fa-github-square" />
+          <div> Github </div>
+        </v-btn>
+        <v-btn class="ma-2" outlined color="black">
+          <v-icon small left class="fab fa-linkedin" />
+          <div> LinkedIn </div>
+        </v-btn>
+        <v-btn class="ma-2" outlined color="black">
+          <v-icon small left class="fas fa-pen-square " />
+          <div> Resume </div>
+        </v-btn>
+      </v-col>
+    </div>
+
+  </v-container>
+
+</template>
+
+<script>
+
+  export default {}
+
+</script>
---
+++ src/components/projects/Projects.vue
@@ -0,0 +1,206 @@
+<template>
+
+  <v-container>
+
+    <v-row>
+      <v-col cols="12">
+        <!-- Start of Project Card -->
+        <v-card class="mx-auto mt-5" max-width="600">
+          <v-img aspect-ratio="2.5" src="" />
+          <v-card-title>Personal Site</v-card-title>
+          <v-divider/>
+          <v-card-text class="text-primary">
+            This is my personal site I made for myself so show what I am currently doing.
+          </v-card-text>
+          <v-divider />
+
+          <!-- Skill Holder -->
+          <v-card-text>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Vue</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Vuetify</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Github Pages</div>
+            </v-chip>
+            <v-divider />
+
+            <!-- Links -->
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="fab fa-github" />
+              <div class="caption font-weight-medium">View The Code</div>
+            </v-chip>
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="far fa-eye" />
+              <div class="caption font-weight-medium">Visit The Project</div>
+            </v-chip>
+          </v-card-text>
+        </v-card>
+        <!-- End of Project Card -->
+        <!-- Start of Project Card -->
+        <v-card class="mx-auto mt-5" max-width="600">
+          <v-img aspect-ratio="2.5" src="" />
+          <v-card-title>Guitarist Toolbox Public API</v-card-title>
+          <v-divider/>
+          <v-card-text class="text-primary">
+            This is my personal site I made for myself so show what I am currently doing.
+          </v-card-text>
+          <v-divider />
+
+          <!-- Skill Holder -->
+          <v-card-text>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Python</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Flask</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">PostgreSQL</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Heroku</div>
+            </v-chip>
+            <v-divider />
+
+            <!-- Links -->
+            <v-chip class="mx-3 mt-5" href="https://github.com/moisesjsanchez/Guitarist-Toolbox">
+              <v-avatar class="fab fa-github" />
+              <div class="caption font-weight-medium">View The Code</div>
+            </v-chip>
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="far fa-eye" />
+              <div class="caption font-weight-medium">Visit The Project</div>
+            </v-chip>
+          </v-card-text>
+        </v-card>
+        <!-- End of Project Card -->
+        <!-- Start of Project Card -->
+        <v-card class="mx-auto mt-5" max-width="600">
+          <v-img aspect-ratio="2.5" src="" />
+          <v-card-title>Fathom Chan </v-card-title>
+          <v-divider/>
+          <v-card-text class="text-primary">
+            This is my personal site I made for myself so show what I am currently doing.
+          </v-card-text>
+          <v-divider />
+
+          <!-- Skill Holder -->
+          <v-card-text>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Python</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Heroku</div>
+            </v-chip>
+            <v-divider />
+
+            <!-- Links -->
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="fab fa-github" />
+              <div class="caption font-weight-medium">View The Code</div>
+            </v-chip>
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="far fa-eye" />
+              <div class="caption font-weight-medium">Visit The Project</div>
+            </v-chip>
+          </v-card-text>
+        </v-card>
+        <!-- End of Project Card -->
+        <!-- Start of Project Card -->
+        <v-card class="mx-auto mt-5" max-width="600">
+          <v-img aspect-ratio="2.5" src="" />
+          <v-card-title>ChordMe (Work in Process)</v-card-title>
+          <v-divider/>
+          <v-card-text class="text-primary">
+            This is my personal site I made for myself so show what I am currently doing.
+          </v-card-text>
+          <v-divider />
+
+          <!-- Skill Holder -->
+          <v-card-text>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Python</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Flask</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">PostgreSQL</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Vue.js</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="20px" src="" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Vuetify</div>
+            </v-chip>
+            <v-divider />
+
+            <!-- Links -->
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="fab fa-github" />
+              <div class="caption font-weight-medium">View The Code</div>
+            </v-chip>
+            <v-chip class="mx-3 mt-5">
+              <v-avatar class="far fa-eye" />
+              <div class="caption font-weight-medium">Visit The Project</div>
+            </v-chip>
+          </v-card-text>
+        </v-card>
+        <!-- End of Project Card -->
+      </v-col>
+    </v-row>
+
+  </v-container>
+
+</template>
+
+<script>
+
+  export default {}
+
+</script>
