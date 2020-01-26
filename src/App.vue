eb7e09d236c42655c1a4242aa17e173cf328d1a9
moisesjsanchez <moijsanchez@gmail.com>
2020-01-26 02:54:46 -0800

    updated components

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <v-content>
       <Home />
       <Projects />
+      <Skills />
+      <About />
+      <Footer />
     </v-content>
   </v-app>
 </template>
@@ -11,12 +14,18 @@
 
 import Home from './components/home/Home'
 import Projects from './components/projects/Projects'
+import About from './components/about/About'
+import Skills from './components/skills/Skills'
+import Footer from './components/footer/Footer'
 
 export default {
 
   components: {
     Home,
-    Projects
+    Projects,
+    About,
+    Skills,
+    Footer
   },
 
   data: () => ({
