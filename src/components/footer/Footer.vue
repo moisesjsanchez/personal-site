8163c000584607059769cd744d910cd4cabba855
moisesjsanchez <moijsanchez@gmail.com>
2020-01-26 02:55:46 -0800

    Created footer section

---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <v-footer dark>
+    <v-col class="text-left ml-5 caption">
+      &#169; 2020 Moises Jornales Sanchez
+    </v-col>
+  </v-footer>
+
+</template>
+
+<script>
+
+  export default {}
+
+</script>
