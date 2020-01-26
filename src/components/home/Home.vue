b5ee429d79293337e2918c9afcaff3a3aa0e5dd0
moisesjsanchez <moijsanchez@gmail.com>
2020-01-26 02:57:06 -0800

    Improved button layout, added overlay, and added more to nav bar

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -6,34 +6,41 @@
     <v-app-bar fixed=True>
       <v-row class="align-center">
         <v-col class="text-center">
+          <v-btn class="mr-3" text>Home</v-btn>
           <v-btn class="mr-3" text>Projects</v-btn>
+          <v-btn class="mr-3" text>Skills</v-btn>
           <v-btn class="mr-3" text>About Me</v-btn>
-          <v-btn class="mr-3" text>Projects</v-btn>
         </v-col>
       </v-row>
     </v-app-bar>
     <section>
       <v-parallax src="@/static/hongkong.jpeg" height="900">
+        <v-overlay absolute=True opacity="0.35">
         <v-row align="center" justify="center">
           <v-col class="text-center" cols="12">
-            <h1 class="display-3 font-weight-thin mb-4 white--text">MOISES J.SANCHEZ</h1>
-            <h3 class="subheading display-1 font-weight-thin white--text">Programmer-Data Enthusiast-Musician</h3>
-            <v-col class="mb-10" cols="12">
-              <v-btn class="ma-5" dark color="black" href="https://github.com/moisesjsanchez">
+            <h1 class="display-3 font-weight-thin mb-4 white--text">MOISES J. SANCHEZ</h1>
+            <h3 class="subheading display-1 font-weight-thin white--text">Programmer-Problem Solver-Musician</h3>
+            <v-col class="mt-5" cols="12">
+              <v-btn class="ma-3" dark color="black" href="https://github.com/moisesjsanchez" target="_blank">
                 <v-icon small left class="fab fa-github-square" />
                 <div> Github </div>
               </v-btn>
-              <v-btn class="ma-5" dark color="black" href="https://www.linkedin.com/in/moisesjsanchez/">
+              <v-btn class="ma-3" dark color="black" href="https://www.linkedin.com/in/moisesjsanchez/" target="_blank">
                 <v-icon small left class="fab fa-linkedin" />
                 <div> LinkedIn </div>
               </v-btn>
-              <v-btn class="ma-5" dark color="black">
-                <v-icon small left class="fas fa-pen-square " />
+              <v-btn class="ma-3" dark color="black" href="mailto:moijsanchez@gmail.com" target="_blank">
+                <v-icon small left class="fas fa-envelope-square" />
+                <div> Email </div>
+              </v-btn>
+              <v-btn class="ma-3" dark color="black" target="_blank">
+                <v-icon small left class="fas fa-pen-square" />
                 <div> Resume </div>
               </v-btn>
             </v-col>
           </v-col>
         </v-row>
+      </v-overlay>
       </v-parallax>
     </section>
   </v-content>
