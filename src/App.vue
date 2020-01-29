62875f0e00ec79638646f40493ec6907c8efc851
moisesjsanchez <moijsanchez@gmail.com>
2020-01-29 01:47:23 -0800

    Added skills and footer. Included ids for router

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,42 @@
 <template>
+
   <v-app>
     <v-content>
-      <Home />
-      <Projects />
-      <Skills />
-      <About />
+      <Home id='home' />
+      <Projects id='projects' />
+      <Skills id='skills' />
+      <About id='about' />
       <Footer />
     </v-content>
   </v-app>
+
 </template>
 
 <script>
 
-import Home from './components/home/Home'
-import Projects from './components/projects/Projects'
-import About from './components/about/About'
-import Skills from './components/skills/Skills'
-import Footer from './components/footer/Footer'
+  import Home from './components/home/Home'
+  import Projects from './components/projects/Projects'
+  import About from './components/about/About'
+  import Skills from './components/skills/Skills'
+  import Footer from './components/footer/Footer'
 
-export default {
+  export default {
+    components: {
+      Home,
+      Projects,
+      About,
+      Skills,
+      Footer
+    }
+  }
 
-  components: {
-    Home,
-    Projects,
-    About,
-    Skills,
-    Footer
-  },
-
-  data: () => ({
-    //
-  }),
-};
 </script>
+
+
+<style>
+
+  .container {
+    max-width: 80vw;
+  }
+
+</style>
