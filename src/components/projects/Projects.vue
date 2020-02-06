c4b85c7161450dfff40c1961f26b972e959e9c6b
moisesjsanchez <moijsanchez@gmail.com>
2020-02-05 22:28:41 -0800

    Added half of images for projects

--- src/components/projects/Projects.vue
+++ src/components/projects/Projects.vue
@@ -11,61 +11,61 @@
       <!-- Start of Project Card -->
       <v-col md="6" xs="12">
         <v-card class="mx-auto mt-5" max-width="700">
-          <v-responsive>
-            <!--  <v-img aspect-ratio="2.5" src="" /> -->
-            <v-card-title>Guitarist Toolbox Public API</v-card-title>
-            <v-divider/>
-            <v-card-text class="text-primary">
-              This is a public RESTful API which allows developers to get various guitar compositional tools which as scales, chord progressions,
-              techniques, and time signatures. Currently this API allows to get types of tools, subtypes of tools, and random
-              tools based on type. Documenation of this API can found in the Github repo linked down below.
-            </v-card-text>
+          <v-img contain="True" aspect-ratio="2.5" src="@/static/discord-orange.png" />
+          <v-divider class="mt-2"/>
+          <v-card-title>Guitarist Toolbox Public API</v-card-title>
+          <v-divider/>
+          <v-card-text class="text-primary">
+            This is a public RESTful API which allows developers to get various guitar compositional tools which as scales, chord progressions,
+            techniques, and time signatures. Currently this API allows to get types of tools, subtypes of tools, and random
+            tools based on type. Documenation of this API can found in the Github repo linked down below.
+          </v-card-text>
+          <v-divider />
+          <!-- Skill Holder -->
+          <v-card-text>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/python.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Python</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img max-height="20px" max-width="30px" src="@/static/flask.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Flask</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/postGreSQL.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">PostgreSQL</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/heroku.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Heroku</div>
+            </v-chip>
             <v-divider />
-            <!-- Skill Holder -->
-            <v-card-text>
-              <v-chip class="mx-3 mb-3">
-                <v-avatar>
-                  <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/python.png" />
-                </v-avatar>
-                <div class="caption font-weight-medium">Python</div>
-              </v-chip>
-              <v-chip class="mx-3 mb-3">
-                <v-avatar>
-                  <v-img max-height="20px" max-width="30px" src="@/static/flask.png" />
-                </v-avatar>
-                <div class="caption font-weight-medium">Flask</div>
-              </v-chip>
-              <v-chip class="mx-3 mb-3">
-                <v-avatar>
-                  <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/postGreSQL.png" />
-                </v-avatar>
-                <div class="caption font-weight-medium">PostgreSQL</div>
-              </v-chip>
-              <v-chip class="mx-3 mb-3">
-                <v-avatar>
-                  <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/heroku.png" />
-                </v-avatar>
-                <div class="caption font-weight-medium">Heroku</div>
-              </v-chip>
-              <v-divider />
-              <!-- Links -->
-              <v-chip class="mx-3 mt-5" href="https://github.com/moisesjsanchez/Guitarist-Toolbox" target="_blank">
-                <v-avatar class="fab fa-github" />
-                <div class="caption font-weight-medium">View The Code</div>
-              </v-chip>
-              <v-chip class="mx-3 mt-5" href="https://guitarist-toolbox.herokuapp.com/" target="_blank">
-                <v-avatar class="far fa-eye" />
-                <div class="caption font-weight-medium">View The Project</div>
-              </v-chip>
-            </v-card-text>
-          </v-responsive>
+            <!-- Links -->
+            <v-chip class="mx-3 mt-5" href="https://github.com/moisesjsanchez/Guitarist-Toolbox" target="_blank">
+              <v-avatar class="fab fa-github" />
+              <div class="caption font-weight-medium">View The Code</div>
+            </v-chip>
+            <v-chip class="mx-3 mt-5" href="https://guitarist-toolbox.herokuapp.com/" target="_blank">
+              <v-avatar class="far fa-eye" />
+              <div class="caption font-weight-medium">View The Project</div>
+            </v-chip>
+          </v-card-text>
         </v-card>
         <!-- End of Project Card -->
       </v-col>
       <v-col md="6" xs="12" cols="12">
         <!-- Start of Project Card -->
         <v-card class="mx-auto mt-5" max-width="700">
-          <!-- <v-img aspect-ratio="2.5" max-height="200px" max-width="200px" src="@/static/discord.png" /> -->
+          <v-img contain="True" aspect-ratio="2.5" src="@/static/discord-orange.png" />
+          <v-divider class="mt-2"/>
           <v-card-title>Fathom Chan </v-card-title>
           <v-divider/>
           <v-card-text class="text-primary">
@@ -103,7 +103,8 @@
       <v-col md="6" xs="12" cols="12">
         <!-- Start of Project Card -->
         <v-card class="mx-auto mt-5" max-width="700">
-          <!-- <v-img aspect-ratio="2.5" src="" /> -->
+          <v-img contain="True" aspect-ratio="2.5" src="@/static/discord-orange.png" />
+          <v-divider class="mt-2"/>
           <v-card-title>ChordMe</v-card-title>
           <v-card-text>Disclaimer: Project Work in Progress. </v-card-text>
           <v-divider/>
@@ -166,14 +167,16 @@
       <v-col md="6" xs="12" cols="12">
         <!-- Start of Project Card -->
         <v-card class="mx-auto mt-5" max-width="700">
-          <!-- <v-img aspect-ratio="2.5" src="" /> -->
+          <v-img contain="True" aspect-ratio="1.7" src="@/static/prog-generator.png" />
+          <v-divider class="mt-2"/>
           <v-card-title>Prog Generator</v-card-title>
           <v-divider/>
           <v-card-text class="text-primary">
             This is a web application what interacts with the Guitarist Toolbox Public API to fetch random critera for creating a potential
-            guitar riff. This idea was created because I wanted a tool to help me create guitar riffs and challenge me to write
-            out of my musical comfort zone. This application is currently written using Vue.js, but soon I am
-            planning on writting the UI using Vuetify to give the application a modern apperance.
+            guitar riff. Currently random critera consist of chord progressions, scales, guitar techniques, and time signatures.
+            This idea was created because I wanted a tool to help me create guitar riffs and challenge me to write out of my
+            musical comfort zone. If you also want to try out writing music with this tool the link to the web application
+            is down below this card! This application is written using Vue.js and Vuetify.
           </v-card-text>
           <v-divider />
 
@@ -206,6 +209,51 @@
             <v-chip class="mx-3 mt-5" href="https://prog-generator.herokuapp.com/" target="_blank">
               <v-avatar class="far fa-eye" />
               <div class="caption font-weight-medium">View The Project</div>
+            </v-chip>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <!-- End of Project Card -->
+      <v-col md="6" xs="12" cols="12">
+        <!-- Start of Project Card -->
+        <v-card class="mx-auto mt-5" max-width="700">
+          <v-img contain="True" aspect-ratio="1.7" src="@/static/personal-website.png" />
+          <v-divider class="mt-2"/>
+          <v-card-title>Personal Website</v-card-title>
+          <v-divider/>
+          <v-card-text class="text-primary">
+            This is currently the website you are visiting. This website was made because I thought it would be fun, and to have a nicer
+            way to show off my past projects than just a github repo. This website houses my past projects, links to recruiters
+            get more information on me, an about me, and secret easter egg hidden somewhere! The website is made with Vue.js
+            and Vuetify and was deployed on Netlify.
+          </v-card-text>
+          <v-divider />
+
+          <!-- Skill Holder -->
+          <v-card-text>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/vue.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Vue.js</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/vuetify.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Vuetify</div>
+            </v-chip>
+            <v-chip class="mx-3 mb-3">
+              <v-avatar>
+                <v-img class="mr-2" max-height="20px" max-width="20px" src="@/static/netlify.png" />
+              </v-avatar>
+              <div class="caption font-weight-medium">Netlify</div>
+            </v-chip>
+            <v-divider />
+            <!-- Links -->
+            <v-chip class="mx-3 mt-5" href="https://github.com/moisesjsanchez/personal-site" target="_blank">
+              <v-avatar class="fab fa-github" />
+              <div class="caption font-weight-medium">View The Code</div>
             </v-chip>
           </v-card-text>
         </v-card>
