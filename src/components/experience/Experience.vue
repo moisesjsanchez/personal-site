3599fab2d7840434bd47c36f08113c05be681995
moisesjsanchez <moijsanchez@gmail.com>
2020-02-22 14:40:57 -0800

    Added experience section

---
+++ src/components/experience/Experience.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <v-container class="mt-10 Projects">
+    <v-row align="center" class="mt-10 pt-5" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="display-3 mb-4 black--text">Work Experience</h1>
+        <v-divider light />
+      </v-col>
+    </v-row>
+    <v-row>
+      <!-- Start of Project Card -->
+      <v-col md="12" xs="12">
+        <v-card class="mx-auto mt-5" max-width="700">
+          <v-img contain="True" aspect-ratio="2.5" src="@/static/honey.png" />
+          <v-divider class="mt-5" />
+          <v-card-title>Store Integration Specialist</v-card-title>
+          <v-card-text class="overline">Santa Barbara, (Feb 2020 - Current)</v-card-text>
+          <v-divider />
+        </v-card>
+        <!-- End of Project Card -->
+      </v-col>
+    </v-row>
+  </v-container>
+
+</template>
+
+<script>
+
+  export default {}
+
+</script>
